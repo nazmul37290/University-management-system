b4b663b89f62d1952f4b5250aca1276f86ebaccc
Nazmul Haque <nazmulnjm@gmail.com>
2024-12-02 15:47:32 +0600

    initialized repo

---
+++ src/app/modules/student/student.service.ts
@@ -0,0 +1,16 @@
+import { StudentModel } from './student.model';
+
+const getAllStudentsFromDB = async () => {
+  const result = await StudentModel.find();
+  return result;
+};
+
+const getSingleStudentFromDB = async (id: string) => {
+  const result = await StudentModel.findOne({ id });
+  return result;
+};
+
+export const StudentServices = {
+  getAllStudentsFromDB,
+  getSingleStudentFromDB,
+};
---
+++ src/app/modules/user/user.services.ts
@@ -0,0 +1,27 @@
+import config from '../../config';
+import { Student } from '../student/student.interface';
+import { StudentModel } from '../student/student.model';
+import { TUser } from './user.interface';
+
+import { userModel } from './user.model';
+
+const createStudentIntoDB = async (password: string, student: Student) => {
+  const userData: Partial<TUser> = {};
+  userData.role = 'student';
+  userData.password = password || (config.default_pass as string);
+  userData.id = '202510001';
+  const newUser = await userModel.create(userData);
+  console.log(newUser);
+  console.log(student);
+  if (Object.keys(newUser).length) {
+    student.id = newUser.id;
+    student.user = newUser._id;
+
+    const newStudent = await StudentModel.create(student);
+    return newStudent;
+  }
+};
+
+export const UserServices = {
+  createStudentIntoDB,
+};
