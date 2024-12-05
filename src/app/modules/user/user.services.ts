0b7762899ceeec57e4e434890331f3a41c897ec7
Nazmul Haque <nazmulnjm@gmail.com>
2024-12-05 14:54:06 +0600

    error in user.service

--- src/app/modules/user/user.services.ts
+++ src/app/modules/user/user.services.ts
@@ -1,4 +1,6 @@
 import config from '../../config';
+import { TAcademicSemester } from '../academicSemester/academicSemester.interface';
+import academicSemesterModel from '../academicSemester/academicSemester.model';
 import { Student } from '../student/student.interface';
 import { StudentModel } from '../student/student.model';
 import { TUser } from './user.interface';
@@ -9,7 +11,18 @@ const createStudentIntoDB = async (password: string, student: Student) => {
   const userData: Partial<TUser> = {};
   userData.role = 'student';
   userData.password = password || (config.default_pass as string);
-  userData.id = '202510001';
+  const admissionSemester = await academicSemesterModel.findById(
+    student.admissionSemester,
+  );
+  const generateStudentUserId = (admissionSemester: TAcademicSemester) => {
+    const currentId = (0).toString();
+    let newId = Number(currentId + 1)
+      .toString()
+      .padStart(4, '0');
+    let userId = `${admissionSemester.year}`;
+  };
+
+  userData.id = generateStudentUserId(admissionSemester);
   const newUser = await userModel.create(userData);
   if (Object.keys(newUser).length) {
     student.id = newUser.id;
